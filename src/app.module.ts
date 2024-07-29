7c0a7e72f076b7821b17e445313bbf9c43316d1c
vkarelina <v.karelina@tg.dunice.net>
2024-07-29 09:49:24 +0300

    sequelize connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize';
 
 @Module({
-  imports: [],
   controllers: [],
   providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_DIALECT as Dialect,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [],
+    }),
+  ],
 })
 export class AppModule {}
